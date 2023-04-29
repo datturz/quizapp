d5129cba746a540c6acd99ca32bedcd7b498daeb
Hidayat Turrahman <hidayattur07@gmail.com>
2023-04-29 19:35:35 +0700

    Revert "first-commit0"
    This reverts commit f8d141190a64c5fa4121c5372d73f8a3d6a4c2db.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import '@/assets/css/style.css'
-import App from './App.vue'
-
-import router from './router'
-import store from './store'
-createApp(App).use(store).use(router)
-.mount('#app')
