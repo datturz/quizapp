e7c5189e39ed5a3689b1a0c9bcdb095eb29b8d85
Hidayat Turrahman <hidayattur07@gmail.com>
2023-04-29 19:36:27 +0700

    Revert "Revert "first-commit0""
    This reverts commit d5129cba746a540c6acd99ca32bedcd7b498daeb.

---
+++ src/views/component/QuizComplete.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-screen h-screen absolute z-30 bg-black bg-opacity-60 flex justify-center items-center">
+        <div class="bg-green-700 p-4 text-center text-white">
+            <p class="font-bold text-2xl">All Done</p>
+            <p class="my-4 font-bold text-3xl">{{percent}}% Score</p>
+            <div class="flex justify-around">
+                <div class="rounded-full py-1 w-28 border cursor-pointer hover:text-black hover:bg-white">Done</div>
+                <div class="rounded-full py-1 w-28 border cursor-pointer hover:text-black hover:bg-white"  @click="$emit('restartQuiz')">Retry</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    props: ['percent',],
+    setup() {
+        
+    },
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+export default {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      primary : '#243c5a', 
+      secondary : {
+        100: '#243c5a',
+      }
+    },
+  },
+  plugins: [],
+}
+
